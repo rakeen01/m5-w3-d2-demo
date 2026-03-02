9dd4b0c3b26d29637dd31566ccf7ecdec7c8b10a
Md Rakeen Murtaza <rakeen.murtaza.12@gmail.com>
2026-03-02 02:22:06 -0800

    commit 1

---
+++ crud-json-server/src/App.js
@@ -0,0 +1,45 @@
+<<<<<<< HEAD
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+=======
+import React from "react";
+
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        loading: false,
+        alldata: [],
+        singledata: {
+          title: "",
+          author: ""
+        }
+      };
+    }
+  }
+
+export default App;
+>>>>>>> 7e4cc7c (setup react project)
